65bd5a2462620d88cf066b7a4edf6ebcff68e4ce
Lee-Seungje <moondgod@gmail.com>
2024-05-16 10:58:29 +0900

    Add loginPage

---
+++ src/pageContainer/LoginPage.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-8 rounded shadow-md w-80">
+        <h2 className="text-2xl font-semibold mb-4">로그인</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
