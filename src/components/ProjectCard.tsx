bfcd65ff3998616f9777521b57a162c9dc102cc2
Lee-Seungje <moondgod@gmail.com>
2024-05-16 12:33:14 +0900

    Update tag

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,11 +16,13 @@ const ProjectCard: React.FC<CollectionType> = ({
     <div className="bg-white shadow-md mb-4">
       <div className="border-b px-4 py-2 flex justify-between items-center">
         <div>
-          <h3 className="text-lg font-semibold text-gray-500">{name}</h3>
-          <span className="text-lg font-semibold text-gray-500">
-            {numberOfImages}
-          </span>
-          <span className="text-lg font-semibold text-gray-500">{created}</span>
+          <p className="text-lg font-semibold text-gray-500">name: {name}</p>
+          <p className="text-lg font-semibold text-gray-500">
+            numberOfImages: {numberOfImages}
+          </p>
+          <p className="text-lg font-semibold text-gray-500">
+            created: {created}
+          </p>
         </div>
         <button
           onClick={() => setIsOpen((prev) => !prev)}
@@ -36,7 +38,7 @@ const ProjectCard: React.FC<CollectionType> = ({
               <tr>
                 <th className="border px-4 py-2 text-gray-500">id</th>
                 <th className="border px-4 py-2 text-gray-500">ontologyName</th>
-                <th className="border px-4 py-2 text-gray-500">생성</th>
+                <th className="border px-4 py-2 text-gray-500">created</th>
               </tr>
             </thead>
             <tbody>
